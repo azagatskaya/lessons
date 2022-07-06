32e0c3ef0453fd8bd966354271eff0718be38d48
Alya Zagatskaya <alya.zagacki@gmail.com>
2022-07-06 16:46:39 +0300

    css

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import './App.css';
+import './App.scss';
 import React from 'react';
 import Cat from './components/Cat.jsx';
 
 const cats = [
   {
   name: 'Мурзик',
-  url: 'https://downloader.disk.yandex.ru/preview/c24aadca86f5c5282179ca62de862a5be6b594115cebb1a61550fad2188afdad/62bb7366/Vx8jZhfFbyX_AUlwN33EH4a5MAMvOJXT-RU8sPtkiRh31zIFLCWSBHxwB85EaRZEq_J6bpmRyOJonT3VoXnDag%3D%3D?uid=0&filename=%D0%9A%D0%BE%D1%828.jpg&disposition=inline&hash=&limit=0&content_type=image%2Fjpeg&owner_uid=0&tknv=v2&size=1131x947'
+  url: 'https://oir.mobi/uploads/posts/2021-04/1619619348_59-oir_mobi-p-samie-milie-kotiki-zhivotnie-krasivo-foto-65.jpg'
   }, {
     name: 'Васька',
     url: 'https://avatars.mds.yandex.net/get-altay/2755030/2a000001720e14d15a4a29db8a76197c5bd5/XXXL'
   }, {
     name: 'Пушок',
-    url: 'https://avatars.mds.yandex.net/get-altay/2815220/2a000001720e13d98bb173408c359c612c5a/XXXL'
+    url: 'https://oir.mobi/uploads/posts/2021-04/1619619315_19-oir_mobi-p-samie-milie-kotiki-zhivotnie-krasivo-foto-23.jpg'
 }
 ]
 
--- src/components/Cat.jsx
+++ src/components/Cat.jsx
@@ -1,14 +1,16 @@
-import './Cat.css';
+import "./Cat.scss";
 
-function Cat(props){
-	return (
-		<div className='cat-card'>
-        <img src={props.url} alt='img'></img>
-        <div className='cat-name'>{props.name}</div>
-        <div className='cat-text'>Замечательный ласковый котик станет украшением вашего дома</div>
-        <button>Взять домой</button>
-		</div>
-	)
+function Cat(props) {
+  return (
+    <div className="cat-card">
+      <img src={props.url} alt="img"></img>
+      <div className="cat-name">{props.name}</div>
+      <div className="cat-text">
+        Замечательный ласковый котик станет украшением вашего дома
+      </div>
+      <button className="cat-btn">Взять домой</button>
+    </div>
+  );
 }
 
 export default Cat;
